e18db727dcdde1a9cb98986c523809ee7fe9788f
Supuky <saito.hajime0112@gmail.com>
2023-11-06 11:51:27 +0900

    認証機能追加

---
+++ auth.ts
@@ -0,0 +1,58 @@
+// import { signIn } from '@/auth';
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { sql } from '@vercel/postgres';
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+
+// export async function authenticate(
+//     prevState: string | undefined,
+//     formData: FormData,
+//   ) {
+//     try {
+//       await signIn('credentials', Object.fromEntries(formData));
+//     } catch (error) {
+//       if ((error as Error).message.includes('CredentialsSignin')) {
+//         return 'CredentialSignin';
+//       }
+//       throw error;
+//     }
+//   }
+ 
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User>`SELECT * from USERS where email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
+ 
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+ 
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+ 
+          if (passwordsMatch) return user;
+        }
+ 
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
+});
